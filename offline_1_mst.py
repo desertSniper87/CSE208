00b7241ecff3804deff66225b05cce50e8eeb06c
desertSniper87 <torsho@localhost.localdomain>
2017-09-21 02:29:21 +0600

    offline_1_create_graph

--- offline_1_mst.py
+++ offline_1_mst.py
@@ -1,31 +1,30 @@
 from collections import defaultdict
 
-import sys
-
 if __name__ == '__main__':
     f = open('input_offline_1.txt')
 
-    parts = f.readline().rstrip().split(" ")
-    if "" in parts:
-        parts.remove("")
-    print(parts)
-    num_of_vert, num_of_edge = int(parts[0]), int(parts[1])
+    num_of_vert = int(f.readline())
+    num_of_edge = int(f.readline())
 
-    graph = defaultdict(list)
-    for i in range(1,num_of_t+1):
-        graph[i] = []
+    graph = defaultdict(dict)
+    for i in range(1, num_of_vert+1):
+        graph[i] = {}
 
     # print(graph)
-    for i in range (0, num_of_edge):
-        line = input().rstrip().split(" ")
-        # line = f.readline().rstrip().split(" ")
-        if "" in line:
-            line.remove("")
+    # for i in range(0, num_of_edge):
+    # line = input().rstrip().split(" ")
+    # line = f.readline().rstrip().split(" ")
+    # print(line)
+    # if "" in line:
+    # line.remove("")
+    # print(line)
+    # graph[int(line[0])].append(int(line[1]))
+    # print(graph)
+
+    for _ in range(0, num_of_edge):
+        line = f.readline().rstrip().split(" ")
         # print(line)
-        graph[int(line[0])].append(int(line[1]))
+        graph[int(line[0])][int(line[1])] = int(line[2])
         # print(graph)
 
-
-
-
-
+    print(graph)
