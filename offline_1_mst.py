07a67994b5df88e60abaa6a4cc574d30cfbccf27
desertSniper87 <torsho@localhost.localdomain>
2017-09-21 13:17:07 +0600

    offline_1_preli_results

--- offline_1_mst.py
+++ offline_1_mst.py
@@ -1,5 +1,44 @@
 from collections import defaultdict
 
+import sys
+
+
+def prim(graph):
+    # print(graph)
+    mst = defaultdict(dict)
+    weights = {}
+    # source = 1
+    # mst[source] = []
+    # neighbors = []
+    for i in graph:
+        mst[i] = []
+        if (graph[i]!={}):
+            # print(graph[i])
+            min_weight = sys.maxsize
+            min_key = 0
+            for key in graph[i]:
+                # print(graph[i][key])
+                if graph[i][key]<min_weight:
+                    min_weight = graph[i][key]
+                    min_key = key
+                # for val in graph[i][key]:
+                    # print(val)
+                    # if graph[i][key] < min_weight:
+                        # min_weight = graph[i][key]
+                        # print (graph[i])
+                        # print (graph[i][key])
+            # print(min_weight)
+            # print(min_key)
+            mst[i].append(min_key)
+            weights[i, min_key] = min_weight
+            # print(mst)
+            # print(weights)
+    return mst
+
+
+    # for
+
+
 if __name__ == '__main__':
     f = open('input_offline_1.txt')
 
@@ -27,4 +66,7 @@ if __name__ == '__main__':
         graph[int(line[0])][int(line[1])] = int(line[2])
         # print(graph)
 
-    print(graph)
+    mst = prim(graph)
+    print(mst)
+
+    # print(graph)
