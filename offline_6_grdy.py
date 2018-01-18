5718f7bd260edf6b35069e019d098947c71a6f56
desertSniper87 <torsho@localhost.localdomain>
2018-01-18 11:52:01 +0600

    offline 6 first phase complete

--- offline_6_grdy.py
+++ offline_6_grdy.py
@@ -2,32 +2,29 @@ import queue
 
 
 class Node:
-    def __init__(self, cargo, left=None, right=None):
-
-        self.cargo = cargo
+    def __init__(self, left=None, right=None):
         self.left  = left
         self.right = right
 
-    def __str__(self):
-        return str(self.cargo)
+def traverse(node, prefix="", code={}):
+    """TODO: Docstring for traverse.
 
-def traverse(node, key, value, code=""):
-    print(node)
-    print(type(node))
-    print(code)
-    code = ""
-    if (type(node)==tuple):
-        print("test")
-        if (node[1]==key):
-            return code
-        else:
-            return
-    elif value< node.cargo:
-        code+("1")
-        traverse(node.left, key, value)
-    elif value> node.cargo:
-        code+("0")
-        traverse(node.right, key, value)
+    :node: tuple consisting of a Value and a Node object
+    :prefix: 0 or 1 for each traversal
+    :code: dictionary 
+    :returns: code : a dictionary containing codewords
+
+    """
+    if (isinstance(node[1].left[1], Node)):
+        traverse(node[1].left, prefix+"0", code)
+    else:
+        code[node[1].left[1]] = prefix+"0"
+
+    if (isinstance(node[1].right[1], Node)):
+        traverse(node[1].right, prefix+"1", code)
+    else:
+        code[node[1].right[1]] = prefix+"1"
+
     return code
 
 
@@ -36,27 +33,22 @@ if __name__ == '__main__':
     num_of_tests = int(f.readline())
     for _ in range(num_of_tests):
         n = int(f.readline())
-        freq_dic = {}
         freq_q = queue.PriorityQueue()
         for i in range(n):
             line = f.readline().rstrip().split(" ")
-            freq_dic[line[0]] = int(line[1])
             freq_q.put((int(line[1]), line[0])) 
         
         while(freq_q.qsize()>1):
             left = freq_q.get()
             right = freq_q.get()
-            node = Node(left[0]+right[0], left, right)
+            node = Node(left, right)
             freq_q.put((left[0]+right[0], node))
 
-        root = (freq_q.get())[1]
-        # print(root.cargo)
+        root = (freq_q.get())
+        code_dic = traverse(root)
 
-
-        # print(freq_dic)
-        # for value in sorted(freq_dic, key=freq_dic.get, reverse=False):
-        for key, value in freq_dic.items():
-            traverse(root, key, value)
+        for key in sorted(code_dic):
+            print(key, code_dic[key])
 
             
         
