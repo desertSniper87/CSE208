88c58f460c926fd671a17b26088edb1a38923c29
desertSniper87 <torsho@localhost.localdomain>
2018-01-18 12:25:31 +0600

    completed offline 6

--- offline_6_grdy_b1.py
+++ offline_6_grdy_b1.py
@@ -1,28 +1,21 @@
-
-
 if __name__ == '__main__':
-    f = open('offline_6_input.txt')
+    f = open('offline_6_b1_input.txt')
     num_of_tests = int(f.readline())
     for _ in range(num_of_tests):
         n = int(f.readline())
-        freq_q = queue.PriorityQueue()
+        coin_value_dict = {}
+        coin_value_list = []
+        line = f.readline().rstrip().split(" ")
         for i in range(n):
-            line = f.readline().rstrip().split(" ")
-            freq_q.put((int(line[1]), line[0])) 
-        
-        while(freq_q.qsize()>1):
-            left = freq_q.get()
-            right = freq_q.get()
-            node = Node(left, right)
-            freq_q.put((left[0]+right[0], node))
+            coin_value_list.append(int(line[i]))
 
-        root = (freq_q.get())
-        code_dic = traverse(root)
+        coin_value_list = sorted(coin_value_list, reverse=True)
+        change = int(f.readline().rstrip())
+        for i in coin_value_list:
+            coin_value_dict[i] = 0
+            while i<=change:
+                coin_value_dict[i] = coin_value_dict[i] + 1
+                change = change - i
 
-        for key in sorted(code_dic):
-            print(key, code_dic[key])
-
-            
-        
-            
-
+        for key in coin_value_dict:
+            print(key," ", coin_value_dict[key])
